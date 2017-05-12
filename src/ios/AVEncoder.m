549e4296cbf1df037a8ee9b6b8c591fb4f565987
masterwebdev <masterwebdev@gmail.com>
2017-05-13 02:42:53 +0300

    f

--- src/ios/AVEncoder.m
+++ src/ios/AVEncoder.m
@@ -16,6 +16,7 @@
 #import <ImageIO/CGImageDestination.h>
 #import <MobileCoreServices/UTCoreTypes.h>
 #import <objc/message.h>
+#import "SDAVAssetExportSession.h"
 
 #ifndef __CORDOVA_4_0_0
 #import <Cordova/NSData+Base64.h>
@@ -28,36 +29,79 @@
     CDVPluginResult* pluginResult = nil;
     NSString* path = [command.arguments objectAtIndex:0];
     NSString* start_position = [command.arguments objectAtIndex:1];
-    NSString* end_position = [command.arguments objectAtIndex:1];
+    NSString* end_position = [command.arguments objectAtIndex:2];
     
     int start_pos=[start_position intValue];
     int end_pos=[end_position intValue];
     
+    NSURL *url = [NSURL fileURLWithPath:path];
+
+    NSString *theFileName = [[url lastPathComponent] stringByDeletingPathExtension];
     
-    NSLog(@"PATHHHHHHHHHHH: %@", path);
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
     
-    NSURL *url = [NSURL URLWithString:path];
+    NSString *UUID = [[NSUUID UUID] UUIDString];
     
-    AVAsset *asset = [AVAsset assetWithURL:url];
-    AVAssetImageGenerator *imageGenerator = [[AVAssetImageGenerator alloc]initWithAsset:asset];
-    CMTime time = [asset duration];
-    time.value = 1 ;
-    CGImageRef imageRef = [imageGenerator copyCGImageAtTime:time actualTime:NULL error:NULL];
-    UIImage *image1 = [UIImage imageWithCGImage:imageRef];
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSString *finalVideoURLString = [documentsDirectory stringByAppendingPathComponent:[NSString stringWithFormat:@"%@_%@.mp4", theFileName,UUID]];
+    NSURL *outputVideoUrl = ([[NSURL URLWithString:finalVideoURLString] isFileURL] == 1)?([NSURL URLWithString:finalVideoURLString]):([NSURL fileURLWithPath:finalVideoURLString]); // Url Should be a file Url, so here we check and convert it into a file Url
     
-    NSData *imageData = UIImageJPEGRepresentation(image1, 1.0);
     
-    NSLog(@"%@", imageData);
+    SDAVAssetExportSession *compressionEncoder = [SDAVAssetExportSession.alloc initWithAsset:[AVAsset assetWithURL:url]]; // provide inputVideo Url Here
+    compressionEncoder.outputFileType = AVFileTypeMPEG4;
+    compressionEncoder.outputURL = outputVideoUrl; //Provide output video Url here
     
-    NSString *base64String = [imageData base64EncodedStringWithOptions:0];
-    NSLog(@"BASE64: %@", base64String);
+    CMTime start = CMTimeMakeWithSeconds(start_pos, 600); // you will modify time range here
+    CMTime duration = CMTimeSubtract(CMTimeMakeWithSeconds(end_pos, 600), start);
+    CMTimeRange range = CMTimeRangeMake(start, duration);
+    compressionEncoder.timeRange = range;
     
-    CDVPluginResult* result = [CDVPluginResult
-                               resultWithStatus:CDVCommandStatus_OK
-                               messageAsString:base64String];
+    compressionEncoder.videoSettings = @
+    {
+    AVVideoCodecKey: AVVideoCodecH264,
+    AVVideoWidthKey: @1280,   //Set your resolution width here
+    AVVideoHeightKey: @720,  //set your resolution height here
+    AVVideoCompressionPropertiesKey: @
+        {
+        AVVideoAverageBitRateKey: @1000000, // Give your bitrate here for lower size give low values
+        AVVideoProfileLevelKey: AVVideoProfileLevelH264High40,
+        },
+    };
+    compressionEncoder.audioSettings = @
+    {
+    AVFormatIDKey: @(kAudioFormatMPEG4AAC),
+    AVNumberOfChannelsKey: @2,
+    AVSampleRateKey: @44100,
+    AVEncoderBitRateKey: @128000,
+    };
     
-    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+    [compressionEncoder exportAsynchronouslyWithCompletionHandler:^
+     {
+         if (compressionEncoder.status == AVAssetExportSessionStatusCompleted)
+         {
+             NSLog(@"Compression Export Completed Successfully: %@", finalVideoURLString);
+
+             CDVPluginResult* result = [CDVPluginResult
+                                        resultWithStatus:CDVCommandStatus_OK
+                                        messageAsString:finalVideoURLString];
+             
+             [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+             
+         }
+         else if (compressionEncoder.status == AVAssetExportSessionStatusCancelled)
+         {
+             NSLog(@"Compression Export Canceled");
+         }
+         else
+         {
+             NSLog(@"Compression Failed");
+             
+         }
+     }];
+    
 }
+
 
 @end
 
